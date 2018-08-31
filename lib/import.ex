bad503e4a0031f8c658fa1a15d3f6c9c0a2feaef
Henrique Almeida <henrique_af1@msn.com>
2018-08-31 11:48:12 -0300

    first commit

---
+++ lib/import.ex
@@ -0,0 +1,13 @@
+defmodule Csv.Import do
+  alias Csv.RecordStream
+
+  def call(input_path, schema: schema, headers: headers) do
+    {:ok, device} = File.open(input_path)
+    {:ok, stream} = RecordStream.new(device, headers: headers, schema: schema)
+
+    # stream |> Enum.each(&Csv.Repo.insert/1) #sync approach
+    stream
+    |> Task.async_stream(Csv.Repo, :insert, [], max_concurrency: 10) #async approach
+    |> Enum.to_list
+  end
+end
---
+++ test/support.exs
@@ -0,0 +1,4 @@
+# Structs
+defmodule Csv.FixtureSchemas.Site do
+  defstruct name: '', url: ''
+end
